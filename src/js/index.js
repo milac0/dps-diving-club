657c5e4e394aa69b70b664d15eddcc4de24715a3
unknown <milac0@hotmail.com>
2021-01-23 17:51:43 +0100

    first commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,51 +1,72 @@
-import $ from 'jquery';
+import $ from "jquery";
 
-(function($) {
-    // Use this variable to set up the common and page specific functions. If you
-    // rename this variable, you will also need to rename the namespace below.
-    var Sage = {
-        // All pages
-        'common': {
-            init: function() {
-            },
-            finalize: function() {
+(function ($) {
+  // Use this variable to set up the common and page specific functions. If you
+  // rename this variable, you will also need to rename the namespace below.
+  var Sage = {
+    // All pages
+    common: {
+      init: function () {},
+      finalize: function () {
+        console.log("[Ok!]");
+      },
+    },
+  };
 
-                console.log("[Ok!]")
-            }
-        }
-    };
+  // The routing fires all common scripts, followed by the page specific scripts.
+  // Add additional events for more control over timing e.g. a finalize event
+  var UTIL = {
+    fire: function (func, funcname, args) {
+      var fire;
+      var namespace = Sage;
+      funcname = funcname === undefined ? "init" : funcname;
+      fire = func !== "";
+      fire = fire && namespace[func];
+      fire = fire && typeof namespace[func][funcname] === "function";
 
-    // The routing fires all common scripts, followed by the page specific scripts.
-    // Add additional events for more control over timing e.g. a finalize event
-    var UTIL = {
-        fire: function(func, funcname, args) {
-            var fire;
-            var namespace = Sage;
-            funcname = (funcname === undefined) ? 'init' : funcname;
-            fire = func !== '';
-            fire = fire && namespace[func];
-            fire = fire && typeof namespace[func][funcname] === 'function';
+      if (fire) {
+        namespace[func][funcname](args);
+      }
+    },
+    loadEvents: function () {
+      // Fire common init JS
+      UTIL.fire("common");
+      // Fire page-specific init JS, and then finalize JS
+      $.each(document.body.className.replace(/-/g, "_").split(/\s+/), function (
+        i,
+        classnm
+      ) {
+        UTIL.fire(classnm);
+        UTIL.fire(classnm, "finalize");
+      });
 
-            if (fire) {
-                namespace[func][funcname](args);
-            }
-        },
-        loadEvents: function() {
-            // Fire common init JS
-            UTIL.fire('common');
-            // Fire page-specific init JS, and then finalize JS
-            $.each(document.body.className.replace(/-/g, '_').split(/\s+/), function(i, classnm) {
-                UTIL.fire(classnm);
-                UTIL.fire(classnm, 'finalize');
-            });
+      // Fire common finalize JS
+      UTIL.fire("common", "finalize");
+      //$(#sh);
+    },
+  };
 
-            // Fire common finalize JS
-            UTIL.fire('common', 'finalize');
-            //$(#sh);  
-        }
-    };
+  // Load Events
+  $(document).ready(UTIL.loadEvents);
 
-    // Load Events
-    $(document).ready(UTIL.loadEvents);
+  var options = {
+    // vertical offset in px before element is first unpinned
+    offset: 200,
+  };
 
+  //   $(".js-headroom").headroom(options);
+
+  $(".burger-menu__icon").on("click", function () {
+    $(".burger-menu").toggleClass("burger-menu--opened");
+    $(".icon__line--second").toggleClass("icon__line--opened");
+    $(".content-overlay").toggleClass("content-overlay--on");
+    $("header").toggleClass("header--opened");
+  });
+
+  $(".content-overlay").on("click", function () {
+    $(".content-overlay").removeClass("content-overlay--on");
+    $(".icon__line--second").removeClass("icon__line--opened");
+    $(".burger-menu").removeClass("burger-menu--opened");
+    $("header").removeClass("header--opened");
+  });
 })(jQuery); // Fully reference jQuery after this point.
